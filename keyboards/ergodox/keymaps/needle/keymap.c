0a895dccadb5540e0b07f4b756978b62d9ee10eb
Kenji Iguchi <versioncontrol@heistak.com>
2017-03-04 02:05:09 +0900

    Tweak to make kana/eisu work in both Mac and Windows

--- keyboards/ergodox/keymaps/needle/keymap.c
+++ keyboards/ergodox/keymaps/needle/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|  :*  |           |  \_  |------+------+------+------+------+--------|
  * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |/ Ctrl| RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | LCtrl|  '"  | Left | Right|Eisuu |                                       |MacKna|  Up  | Down |   [  |   ]  |
+ *   | LCtrl|  '"  | Left | Right|Muhenk|                                       |Henkan|  Up  | Down |   [  |   ]  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | Ins  | Del  |       | Home | End  |
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         CTL_T(KC_TAB),  KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
         KC_BSPC,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   JA_CLON,
-        KC_LCTL,        KC_F17,       KC_LEFT,KC_RGHT,KC_LANG2,
+        KC_LCTL,        KC_F17,       KC_LEFT,KC_RGHT,KC_MHEN,
                                                       KC_INS, KC_DEL,
                                                               KC_ZKHK,
                                         GUI_T(KC_SPC),KC_LALT,KC_CAPS,
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              TG(SYMB),    KC_Y,   KC_U,   KC_I,   KC_O,     KC_P,     JA_AT,
                           KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),GUI_T(KC_ENT),
              JA_ENUN,     KC_N,   KC_M,   KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
-                                  KC_LANG1,KC_UP, KC_DOWN,JA_LBRC,CTL_T(JA_RBRC),
+                                  KC_HENK,KC_UP, KC_DOWN,JA_LBRC,CTL_T(JA_RBRC),
              KC_HOME,        KC_END,
              KC_PGUP,
              KC_PGDN,KC_SPC, KC_F16
